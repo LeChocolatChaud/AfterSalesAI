8afe6f0719c4b42acea891d2b7d349d0ec202bba
LeChocolatChaud <mazihan1024@hotmail.com>
2023-09-23 23:02:13 +0800

    front end changes

--- web/scripts/index.js
+++ web/scripts/index.js
@@ -1,15 +1,34 @@
-$(function(){$("button#send-button").on("click", function(_e) {
+$(function () {
+  function stack(text, role) {
+    let element = text + "</div>";
+    switch (role) {
+      case "AI":
+        element = `<div class="message ai-message">${element}`;
+        break;
+      case "user":
+        element = `<div class="message user-message">${element}`;
+        break;
+      default:
+        throw new Error("Unsupported role.");
+    }
+    $("div#message-stacking").append(element);
+  }
+  $("button#send-button").on("click", function (_e) {
+    if ($("input#input-text").val() === "") return;
+    stack($("input#input-text").val(), "user");
     $.ajax({
-        url: `http://${window.location.hostname}:8001/send`,
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        data: JSON.stringify({
-            "input": $("textarea#input-text").val()
-        }),
-        success: function(data) {
-            $("textarea#output-text").val(data.text);
-        }
+      url: `http://${window.location.hostname}:8001/send`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: JSON.stringify({
+        input: $("input#input-text").val(),
+      }),
+      success: function (data) {
+        stack(data.text, "AI");
+      },
     });
-});});
+    $("input#input-text").val("");
+  });
+});
